817eef14dfd2601edbef7010fe5cf7d0d092590f
krishnakesari <bkesari@outlook.com>
2020-12-22 12:51:45 -0500

    Completed Map with markers

--- src/components/maps.js
+++ src/components/maps.js
@@ -1,17 +1,31 @@
-
-import React , {useState} from 'react';
-import ReactMapGL from 'react-map-gl';
-import LocationMarker from "./LocationMarker";
+import React , {useState, useEffect} from 'react';
+import ReactMapGL, {Marker, Popup} from 'react-map-gl';
+import * as parkDate from "../data/data.json";
 
 export default function Map(){
 
 const [viewport, setViewport] = useState({
-    latitude: 38.897957,
-    longitude: -77.036560,
+    latitude: 45.4211,
+    longitude: -75.6903,
     width: '100vw',
     height: '100vh',
-    zoom: 12
+    zoom: 10
   });
+
+const [selectedPark, setSelectedPark] = useState(null);
+
+useEffect(() => {
+    const listener = (e) => {
+        if(e.key === "Escape") {
+            setSelectedPark(null);
+        }
+    };
+    window.addEventListener("keydown", listener);
+
+    return () => {
+        window.removeEventListener("keydown", listener);
+    }
+}, []);
 
     return (
         <div>
@@ -21,7 +35,33 @@ const [viewport, setViewport] = useState({
                         onViewportChange={viewport => {
                             setViewport(viewport);
                         }}>
-                <LocationMarker latitude={viewport.latitude} longitude={viewport.longitude}/>
+                {parkDate.features.map(park => (
+                     <Marker key={park.properties.PARK_ID}
+                     latitude={park.geometry.coordinates[1]}
+                     longitude={park.geometry.coordinates[0]}
+                     >
+                     <button className= "marker-btn" onClick={(e) => {
+                         e.preventDefault();
+                         setSelectedPark(park);
+                     }}>
+                         <img className= "img" src="/skate.png" alt={"Skate Icon"} />
+                     </button>
+                </Marker>
+                ))}
+                {selectedPark ? (
+                   <Popup className="popup" latitude = {selectedPark.geometry.coordinates[1]}
+                          longitude ={selectedPark.geometry.coordinates[0]}
+                          onClose = {() => {
+                              setSelectedPark(null)
+                          }}
+                   >
+                       <div>
+                           <h4>{selectedPark.properties.NAME}</h4>
+                           <h4>{selectedPark.properties.DESCRIPTIO}</h4>
+                           <h5>{selectedPark.properties.NOTES}</h5>
+                       </div>
+                   </Popup>
+                ) : null}
             </ReactMapGL>
         </div>
     );
